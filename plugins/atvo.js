5539acb49527742c894e063f57a1e31e1a319286
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-09-15 13:10:43 +0530

    Create atvo.js

---
+++ plugins/atvo.js
@@ -0,0 +1,165 @@
+/* Copyright © 2021 TERROR BOY.
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+WHITE DEVIL ----»»» TERROR BOY
+*/
+
+const Ktb = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+
+const Language = require('../language');
+const Lang = Language.getString('Autovideo');
+
+// AUTO VIDEO COMMAND LIST
+
+// THIS PLUGIN IS OWNED BY KARTHIK_TERROR-BOY
+
+ Ktb.addCommand({pattern: 'atvo', fromMe:false , desc: Lang.AV}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+     r_text[1] = "https://i.imgur.com/YRnXdbh.png";
+     r_text[2] = "https://i.imgur.com/YRnXdbh.png";
+   
+    
+    
+    var i = Math.floor(3*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+// EXPORTING COMMAND LIST 
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━✨️WhiteDevil✨️━━━⛦
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 AMAL-DX's 𝐛𝐨𝐭.
+            *DXTROX_V1*
+■□■□■□■□■□■□■□■□■□■□
+📽 *AUTO VIDEO* 📽
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Free fire*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *ff*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manchester United*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manchester*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manju warrier*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Manju* 
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *Whitedevil*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *pubg*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *life*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n ➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  *[LOADING.....]*
+\n\n *[IF DID NOT GET RESULT IT MAY BE BOT IS UNDER MAINTANCE OR ELSE IF YOU USE HANDLERS LIKE(!/,.#)]*
+■□■□■□■□■□■□■□■□■□■□
+═════💢DXTROX_V1💢═════
+▣▣▣▣▣▣▣▣▣ PLUGIN OWNER: Karthik_terror-boy  ▣▣▣▣▣▣▣▣▣▣▣
+`,quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": "◄━━━━━⦁⦁ *DXTROX_V1* ⦁⦁━━━━━►", "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": fs.readFileSync('./DXTROX/src/img/dx.png')}}}});
+
+}));
