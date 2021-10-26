699ab33bbb316082fbbe4bb2878730e9c3c09767
Amal-dx <88432886+amal-dx@users.noreply.github.com>
2021-10-26 20:21:10 +0530

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,17 +1,18 @@
 /* Codded by @phaticusthiccy
 Telegram: t.me/phaticusthiccy
-Instagram: www.instagram.com/kyrie.baran
+Instagram: www.instagram.com/kyrie.baran 
 */
 
-const Asena = require('../events');
+const MyPnky = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const Config = require('../config');
 
 const Language = require('../language');
 const Lang = Language.getString('tagall');
 
+if (Config.STANDPLK == 'off' || Config.STANDPLK == 'OFF') {
 if (Config.WORKTYPE == 'private') {
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
+    MyPnky.addCommand({pattern: 'tagadmin$', fromMe: true, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
@@ -25,7 +26,7 @@ if (Config.WORKTYPE == 'private') {
     }));
 }
 else if (Config.WORKTYPE == 'public') {
-    Asena.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+    MyPnky.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
         let grup = await message.client.groupMetadata(message.jid);
         var jids = [];
         mesaj = '';
@@ -37,4 +38,20 @@ else if (Config.WORKTYPE == 'public') {
         });
         await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
     }));
+}
+
+else if (Config.WORKTYPE == 'admin') {
+    MyPnky.addCommand({pattern: 'tagadmin$', fromMe: false, desc: Lang.TAGADMİN}, (async (message, match) => {
+        let grup = await message.client.groupMetadata(message.jid);
+        var jids = [];
+        mesaj = '';
+        grup['participants'].map(async (uye) => {
+            if (uye.isAdmin) {
+                mesaj += '👮‍♂️@' + uye.id.split('@')[0] + '\n';
+                jids.push(uye.id.replace('c.us', 's.whatsapp.net'));
+            }
+        });
+        await message.client.sendMessage(message.jid,mesaj, MessageType.extendedText, {contextInfo: {mentionedJid: jids}, previewType: 0})
+    }));
+}
 }
