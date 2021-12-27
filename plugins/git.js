53ef3b74c7dba66e65d69b5263b23868f34c21d1
Karthik_terror-boy <85664936+terror-boy@users.noreply.github.com>
2021-12-27 09:44:00 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,31 +1,59 @@
-const Asena = require('../events');
-const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+
+
+
+/*  
+RE CODE - KTB 
+*/
+
+const Ktb = require('../events');
+const {WAConnection , MessageType} = require('@adiwajshing/baileys');
+const {spawnSync} = require('child_process');
+const Config = require('../config');
+const config = require('../config');
+const util = require('util')
+const parseInt = require('parse-int');
+const chalk = require('chalk');
+const conn = new WAConnection();
+
+const White = require('../white');
 const axios = require('axios');
 
 const Language = require('../language');
-const Lang = Language.getString('wallpaper');
+const Lang = Language.getString('system_stats');
 
-Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
-    var r_text = new Array ();
+const conf = require('../config');
+let wk = conf.WORKTYPE == 'public' ? false : true
+
+var SYSDTXT =  '😇 ᴛʜᴀɴᴋ ʏᴏᴜ ꜰᴏʀ ᴜꜱɪɴɢ *ᴡʜɪᴛᴇᴅᴇᴠɪʟ ʙᴏᴛ* 😇'
+
+var VER = '😍 *꧁•⊹٭𝙰𝚛𝚎 𝚈𝚘𝚞 𝚑𝚊𝚙𝚙𝚢٭⊹•꧂* 😍'
+var MSG = '*bot setting video : loading soon*\n\n*githublink : https://github.com/amal-dx/DXTROX_V1*\n\n\n*audio commads : https://github.com/amal-dx/DXTROX_V1/tree/master/uploads*'
+
+  Ktb.addCommand({pattern: 'git', fromMe: wk, desc: Lang.ALIVE_DESC}, (async (message, match) => {
     
+    var logo = await axios.get ('https://raw.githubusercontent.com/amal-dx/DXTROX_V1/master/photo/amalser.png', {responseType: 'arraybuffer'})
+    var PIC = Buffer.from(logo.data)
+
+    const media = await message.client.prepareMessage(message.jid, PIC, MessageType.image, { thumbnail: White.tm_b })
+
     
+        
+    const buttons = [
+        {buttonId: VER, buttonText: {displayText: VER }, type: 1},
+        {buttonId: SYSDTXT, buttonText: {displayText: SYSDTXT }, type: 1}
+    ]
+    const buttonMessage = {
+        contentText: MSG,
+        footerText: '© *creater Amal* © \n\n owner- type owner \n\n',
+        buttons: buttons,
+        headerType: 4,
+        imageMessage: media.message.imageMessage    
+    }
+    await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage);
+}))
+
+
+     
+     
    
-  r_text[0] = "http://telegra.ph/file/68809ff84d7cd4447a76e.jpg";
-    
-    
-    var i = Math.floor(1*Math.random())
-
-    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
-
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
-*owner number Wa.me/917736308760*
-
-*bot setting video : loading soon*
-
-*githublink : https://github.com/amal-dx/DXTROX_V1*
-
-*audio commads : https://github.com/amal-dx/DXTROX_V1/tree/master/uploads*
-`}) 
-
-}));
